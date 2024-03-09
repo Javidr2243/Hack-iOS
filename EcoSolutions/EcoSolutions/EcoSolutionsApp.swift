099db85cc1a96039020804ab92725485427d2167
Javier <113366019+Javidr2243@users.noreply.github.com>
2024-03-09 17:36:39 -0600

    Se agrega git ignore

---
+++ EcoSolutions/EcoSolutions/EcoSolutionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoSolutionsApp.swift
+//  EcoSolutions
+//
+//  Created by Javier Davila on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct EcoSolutionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
