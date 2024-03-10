4e344024194449be3b23759b78a3accc589d21a3
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2024-03-10 04:43:52 -0600

    assets added, beginning animation added

--- EcoSolutions/EcoSolutions/ContentView.swift
+++ EcoSolutions/EcoSolutions/ContentView.swift
@@ -9,16 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selectedTab = 2
+    @State private var isLoading = true
     
     // Replace index 0 with actual text from User instance
     var headerTittles = ["Mi Hogar", "Mi Vehiculo", "Bienvenido Sebastian", "Logros", "Perfil"]
     var body: some View {
-        VStack {
-            HeaderView(headerTittle: headerTittles[selectedTab])
-            
-            TabView(selection: $selectedTab) {
-                HomeView(
-                    applianceList: [
+        
+        ZStack {
+            VStack {
+                HeaderView(headerTittle: headerTittles[selectedTab])
+                
+                TabView(selection: $selectedTab) {
+                    HomeView(
+                        applianceList: [
+                            ItemCategory(
+                                id: "Cocina",
+                                items: [
+                                    Item(consumo: 10.0, name: "Estufa", iconName: "stove", quantity: 2, smart: true),
+                                    Item(consumo: 15.0, name: "Licuadora", iconName: "eraser", quantity: 1, smart: false)
+                                ]
+                            ),
+                            ItemCategory(
+                                id: "Aseo",
+                                items: [
+                                    Item(consumo: 8.0, name: "Aspiradora", iconName: "stove", quantity: 1, smart: true),
+                                    Item(consumo: 12.0, name: "Lavadora", iconName: "eraser", quantity: 2, smart: false)
+                                ]
+                            ),
+                            // Add more categories as needed
+                        ]
+                    )
+                        .tabItem {
+                            Label("Home", systemImage: "powerplug")
+                        }
+                        .tag(0)
+                    
+                    CarView(applianceList: [
                         ItemCategory(
                             id: "Cocina",
                             items: [
@@ -33,55 +59,41 @@ struct ContentView: View {
                                 Item(consumo: 12.0, name: "Lavadora", iconName: "eraser", quantity: 2, smart: false)
                             ]
                         ),
-                        // Add more categories as needed
-                    ]
-                )
-                    .tabItem {
-                        Label("Home", systemImage: "powerplug")
-                    }
-                    .tag(0)
-                
-                CarView(applianceList: [
-                    ItemCategory(
-                        id: "Cocina",
-                        items: [
-                            Item(consumo: 10.0, name: "Estufa", iconName: "stove", quantity: 2, smart: true),
-                            Item(consumo: 15.0, name: "Licuadora", iconName: "eraser", quantity: 1, smart: false)
-                        ]
-                    ),
-                    ItemCategory(
-                        id: "Aseo",
-                        items: [
-                            Item(consumo: 8.0, name: "Aspiradora", iconName: "stove", quantity: 1, smart: true),
-                            Item(consumo: 12.0, name: "Lavadora", iconName: "eraser", quantity: 2, smart: false)
-                        ]
-                    ),
-                ])
-                    .tabItem {
-                        Label("Car", systemImage: "car.side")
-                    }
-                    .tag(1)
-                
-                MainMenu()
-                    .tabItem {
-                        Label("Main Menu", systemImage: "house")
-                    }
-                    .tag(2)
-                
-                LeaderBoardView()
-                    .tabItem {
-                        Label("Leader Board", systemImage: "trophy")
-                    }
-                    .tag(3)
-                
-                MapView()
-                    .tabItem {
-                        Label("Profile", systemImage: "person")
-                    }
-                    .tag(4)
+                    ])
+                        .tabItem {
+                            Label("Car", systemImage: "car.side")
+                        }
+                        .tag(1)
+                    
+                    MainMenu()
+                        .tabItem {
+                            Label("Main Menu", systemImage: "house")
+                        }
+                        .tag(2)
+                    
+                    LeaderBoardView()
+                        .tabItem {
+                            Label("Leader Board", systemImage: "trophy")
+                        }
+                        .tag(3)
+                    
+                    MapView()
+                        .tabItem {
+                            Label("Profile", systemImage: "person")
+                        }
+                        .tag(4)
+                }
+                .background(Color("white-dark"))
+                Spacer()
             }
-            .background(Color("white-dark"))
-            Spacer()
+            
+            LoadingPage(isLoading: $isLoading).onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        isLoading = false
+                    }
+                }
+            }
         }
 
     }
